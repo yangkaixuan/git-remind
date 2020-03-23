4d2ea760c6014af4533428ffe25241e7c50e24cd
yangkaixuan5 <yangkaixuan5@jd.com>
2020-03-23 19:57:47 +0800

    fix:优化代码

--- bin/git-verification.js
+++ bin/git-verification.js
@@ -4,7 +4,7 @@
 const {exec} = require('child_process')
 const config = require('../package.json');
 const {gitFn,log} = require('../lib/utils');
-
+const inquirer = require('inquirer');
 
 let mainBranch = 'master' //默认为 master 分支
 const maxBuffer = 100 * 1024 * 1024;
@@ -12,52 +12,52 @@ const scriptName = process.argv[2];
 
 if (config.mainBranch) mainBranch = config.mainBranch;
 
-
+let num = 1;
 async  function main() {    
     let isOk = true; 
     const remotesList = await gitFn.getRemotes();
     if(!remotesList.length){
-        log('red', '请先添加remotes');   
+        log('red', ['×', `  ${num++}.请先添加remotes!`]);   
         isOk = false;
         return isOk;
     }
     const fetchContent = await gitFn.fetch(); 
     if (fetchContent.raw) {
         console.log(fetchContent.raw)
-        log('green', ['√', '  1.fetch success!'])
+        log('green', ['√', `  ${num++}.fetch success!`])
     } else { 
-        log('green', ['√', '  1.nothing to fetch'])
+        log('green', ['√', `  ${num++}.nothing to fetch`])
     }    
     const currentBranch = await gitFn.getCurrentBranch();    
     const mainBranchInfo = await gitFn.getbranchInfo(mainBranch);    
     const isBehind = mainBranchInfo.label.search(/behind(?= \d+\])/ig);	 
     const isAhead = mainBranchInfo.label.search(/ahead(?= \d+\])/ig);   
     if (isBehind === -1 && isAhead === -1) {
-        log('green', ['√', `  2.主干分支:${mainBranch}分支 为最新提交`]);
+        log('green', ['√', `  ${num++}.主干分支:${mainBranch}分支 为最新提交`]);
     }
     if (isBehind !== -1) {
         isOk = false;
-        log('red', [`×   2.${mainBranch}主干分支需要更新`]);
+        log('red', [`×   ${num++}.${mainBranch}主干分支需要更新`]);
     } 
     if (isAhead !== -1) {
-        log('red', [`×   2.注意本地 ${mainBranch} 主干分支没有提交`]);
+        log('red', [`×   ${num++}.注意本地 ${mainBranch} 主干分支没有提交`]);
     }
     
     if (currentBranch !== mainBranch) {
         const mergedList = await gitFn.getbranchmerged();
         const ismerged = mergedList.includes(mainBranch);
         if (ismerged) {
-            log('green', ['√', `  3.当前分支${currentBranch}已经合并了主干分支${mainBranch}`])
+            log('green', ['√', `  ${num++}.当前分支${currentBranch}已经合并了主干分支${mainBranch}`])
         } else {
             isOk = false;
-            log('red', [`×   3.发现当前分支${currentBranch}没有合并主干${mainBranch}分支，是否合并？`]);         
+            log('red', [`×   ${num++}.发现当前分支${currentBranch}没有合并主干${mainBranch}分支，是否合并？`]);         
         }
         const Behind = await gitFn.getCurrentBehind();
         if (Behind === 0) {
-            log('green', ['√', `  4.当前分支${currentBranch}目前没有更新`])
+            log('green', ['√', `  ${num++}.当前分支${currentBranch}目前没有更新`])
         } else {
             isOk = false;
-            log('red', [`×   4.当前分支${currentBranch}目前没有更新`])
+            log('red', [`×   ${num++}.当前分支${currentBranch}目前没有更新`])
         }
     }
     return isOk;
