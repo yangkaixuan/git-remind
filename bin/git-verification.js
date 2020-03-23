af42ff2218cc06655e52fd0cc4b1566457e49af1
yangkaixuan5 <yangkaixuan5@jd.com>
2020-03-23 20:11:38 +0800

    fix:test

--- bin/git-verification.js
+++ bin/git-verification.js
@@ -31,13 +31,31 @@ async  function main() {
     const currentBranch = await gitFn.getCurrentBranch();    
     const mainBranchInfo = await gitFn.getbranchInfo(mainBranch);    
     const isBehind = mainBranchInfo.label.search(/behind(?= \d+\])/ig);	 
-    const isAhead = mainBranchInfo.label.search(/ahead(?= \d+\])/ig);   
+    const isAhead = mainBranchInfo.label.search(/ahead(?= \d+\])/ig);  
+
     if (isBehind === -1 && isAhead === -1) {
         log('green', ['√', `  ${num++}.主干分支:${mainBranch}分支 为最新提交`]);
     }
     if (isBehind !== -1) {
         isOk = false;
-        log('red', [`×   ${num++}.${mainBranch}主干分支需要更新`]);
+        log('red', [`×   ${num++}.${mainBranch} 主干分支需要更新`]);
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: "是否更新主干分支？Y/N",
+                validate(value) {
+                     if(value === 'Y'){
+                         console.log('拉去');
+                     }else{
+                         return true
+                     }
+                }
+            }
+        ])
+        .then(answers => {
+            fileHandle(answers.name)
+        });
     } 
     if (isAhead !== -1) {
         log('red', [`×   ${num++}.注意本地 ${mainBranch} 主干分支没有提交`]);
